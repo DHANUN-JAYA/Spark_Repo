ed0bbed1cef7268d5140b0427975e0927c040d3e
Your Name <you@example.com>
2023-09-09 20:45:42 +0530

    assignment_1

---
+++ src/Assignment_1/driver.py
@@ -0,0 +1,23 @@
+from utils import *
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Spark_Assignment_1').getOrCreate()
+
+#loading user_csv file
+df_user = spark.read.csv('C:/Users/Welcome/PycharmProjects/Spark_Repo/Spark_Repo/resourses/user.csv', header=True)
+
+#loading transaction_csv file
+df_transaction = spark.read.csv('C:/Users/Welcome/PycharmProjects/Spark_Repo/Spark_Repo/resourses/transaction.csv', header=True)
+
+#merging  user_csv and transaction_csv file
+total_df = merge(df_user,df_transaction)
+
+# a) Count of unique locations where each product is sold.
+count_unique_locations(total_df)
+
+# b) Find out products bought by each user.
+products_bought(total_df)
+
+# c) Total spending done by each user on each product.
+total_spending(total_df)
---
+++ src/Assignment_1/utils.py
@@ -0,0 +1,17 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import countDistinct, sum
+import logging
+
+def merge(df_user,df_transaction):
+    total_df=total_df = df_user.join(df_transaction, df_user.user_id == df_transaction.userid )
+    return total_df
+def count_unique_locations(total_df):
+     # a) Count of unique locations where each product is sold.
+     total_df.groupBy("location ").agg(countDistinct("product_description").alias("product_Count")).show()
+def products_bought(total_df):
+    # b) Find out products bought by each user.
+    total_df.groupBy("user_id").agg({"product_description": "collect_list"}).show()
+def total_spending(total_df):
+    # c) Total spending done by each user on each product.
+    user_product_spending = total_df.groupBy(["user_id"]).agg(sum("price").alias("Total_Spending")).show()
